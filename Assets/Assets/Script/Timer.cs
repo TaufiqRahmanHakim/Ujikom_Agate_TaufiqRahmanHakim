f3970a1490669e021772154ea68e3c679939fb8b
Taufiq Rahman Hakim <pxpiksy@gmail.com>
2024-12-18 11:31:54 +0700

    Add Animation GameOver and fix player nabrak animal

--- Assets/Assets/Script/Timer.cs
+++ Assets/Assets/Script/Timer.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
@@ -8,12 +10,23 @@ public class Timer : MonoBehaviour
     public TextMeshProUGUI tmpTimer;
     public TextMeshProUGUI tmpScoreGameOver;
 
+    [SerializeField] private GameObject camera1;
+    [SerializeField] private GameObject camera2;
+
     [SerializeField] private GameObject GameOver;
 
     private bool flagGameOver = false;
 
+    [SerializeField] private Animator animator;
+
 
     [SerializeField] private float timer;
+
+    private void Start()
+    {
+        camera2.SetActive(false);
+        camera1.SetActive(true);
+    }
 
     private void Update()
     {
@@ -22,7 +35,10 @@ public class Timer : MonoBehaviour
 
         if (timer <= 0 && !flagGameOver)
         {
+            camera1.SetActive(false);
+            camera2.SetActive(true);
             GameOver.SetActive(true);
+            animator.SetBool("GameOver", true);
             //Time.timeScale= 0f;
             tmpScoreGameOver.text = "Score : " + Score.Instance.scoreValue;
             flagGameOver = true;
