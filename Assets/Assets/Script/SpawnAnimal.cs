c7c8174922168e07f5a4ba2609cda7ad7d968114
Taufiq Rahman Hakim <pxpiksy@gmail.com>
2024-12-18 10:11:01 +0700

    Spawn Animal logic

--- Assets/Assets/Script/SpawnAnimal.cs
+++ Assets/Assets/Script/SpawnAnimal.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class SpawnAnimal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] animal;
+
+    [SerializeField]private float timerReset;
+
+    private void Update()
     {
-        
+        if(timerReset <= 3f)
+        {
+            timerReset += 1f * Time.deltaTime;
+        }
+        else if(timerReset > 3f)
+        {
+            timerReset = 0;
+            SpawnObject();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        SpawnObject();
+    }
+    private void SpawnObject()
+    {
+        Instantiate(animal[Random.Range(0,3)], new Vector3(Random.Range(20f,-20f),transform.position.y, transform.position.z), Quaternion.identity);
     }
 }
