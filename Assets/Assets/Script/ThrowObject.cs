d511940c59a25221db66fca759ee225c0dfa8c3e
Taufiq Rahman Hakim <pxpiksy@gmail.com>
2024-12-18 10:36:31 +0700

    Set Animator Throw

--- Assets/Assets/Script/ThrowObject.cs
+++ Assets/Assets/Script/ThrowObject.cs
@@ -6,18 +6,24 @@ public class ThrowObject : MonoBehaviour
 {
     [SerializeField] private GameObject throwObject;
     [SerializeField] private Transform targetObject;
+    [SerializeField] private Animator animator;
 
     private void Throw()
     {
         Instantiate(throwObject, gameObject.transform.position, Quaternion.identity);
+        Invoke("animatorSthrowFlag", 0.5f);
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
+            animator.SetBool("Throw",true);
             Debug.Log("fdddf");
             Throw();
         
         }
     }
-
+    private void animatorSthrowFlag()
+    {
+        animator.SetBool("Throw", false);
+    }
 }
